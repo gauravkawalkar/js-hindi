e2444d60bcea4ae861a435fcfe3d335832f5723a
gauravkawalkar <gauravkawalkar09@gmail.com>
2024-06-03 16:50:31 +0000

    We learn about datatypes.

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //as in eariler days js was only upto browser but now it is open so there is new version of js and thiis treat all code as new version
+// alert (3 + 3) it will give error here as we are practising in nodejs and not in browser
+console.log("77"); console.log("88") //dont write this typr of code your code should be readable
+// lets disscuss about the documentation the doc. of js is given by mozilla organization i.e mdn but it is no tthe original doc. it is at tc39.es
+
+
+// there are following datatypes in js number with range 2^53;string;boolean;null (it is stand alone value);undefined;symbol(to find uniqueness)
+// these all are primitive datatypes
+
+//objects. these are aslo one type
+
+
+console.log(typeof "gaurav"); // type of is usedto find datatype of any data
